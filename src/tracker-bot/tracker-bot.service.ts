b75a44a659f24709f2634bbf001a94a5a4af929e
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-11-07 23:59:21 +0100

    returns new4

--- src/tracker-bot/tracker-bot.service.ts
+++ src/tracker-bot/tracker-bot.service.ts
@@ -320,7 +320,7 @@ export class TrackerBotService {
                   },
                   { new: true }, // returns the updated document
                 );
-                if (updateToken.swapsCount === 3) {
+                if (updateToken.swapsCount === 10) {
                   await this.sendTransactionDetails(updateToken);
                   //TODO: change details
                 }
