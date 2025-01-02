27e211103c98a996c0fde9b786da56db4c62dcdb
ceasar28 <emmanuelugochukwuekete@gmail.com>
2025-01-02 21:42:31 +0100

    update1

--- src/soltracker/markups/transactionDetails.ts
+++ src/soltracker/markups/transactionDetails.ts
@@ -10,7 +10,7 @@ export const showTransactionDetails = async (data: any) => {
     tokenBalance,
   } = data;
   // Get the last transaction hash
-  const lastTransactionHash = swapSignatures[0];
+  const lastTransactionHash = swapSignatures[swapSignatures.length - 1];
   function formatTimestamp(isoTimestamp: string): string {
     const date = new Date(isoTimestamp);
 
@@ -25,7 +25,7 @@ export const showTransactionDetails = async (data: any) => {
   // const amount_out = formatNumberWithSuffix(+data.amount0Out);
   return {
     message: `<b>Token Alert 🚨</b>\n<b>Contract Address:</b> 
-<a href="https://solscan.io/token/${tokenContractAddress}">${tokenContractAddress}</a>\n\n<b>Log:</b>\n<b>Token Name✅: </b>${name} (${symbol})\n<b>Amount Bought: </b>${tokenBalance} ${symbol}\n<b>Total Sol spent: </b>${solAmount}(SOL) (~$${usdAmount})\n\n<b>Token Buy Time: </b>${formatedTime}\n<b>first buyHash: </b><a href="https://solscan.io/tx/${lastTransactionHash}">${lastTransactionHash}</a>`,
+<a href="https://solscan.io/token/${tokenContractAddress}">${tokenContractAddress}</a>\n\n<b>Log:</b>\n<b>Token Name✅: </b>${name} (${symbol})\n<b>Amount Bought: </b>${tokenBalance} ${symbol}\n<b>Total Sol spent: </b>${solAmount}(SOL) (~$${usdAmount})\n\n<b>Token Buy Time: </b>${formatedTime}\n<b>last buyHash: </b><a href="https://solscan.io/tx/${lastTransactionHash}">${lastTransactionHash}</a>`,
     // keyboard: [
     //   [
     //     {
